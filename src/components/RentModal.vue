9dbc81eb42b066bb63ca9542da9f6b403544f11c
MUTED <muted32555664@gmail.com>
2021-05-12 22:59:32 +0800

    Design Confirm Page

--- src/components/RentModal.vue
+++ src/components/RentModal.vue
@@ -1,22 +1,45 @@
 <template>
   <ion-modal :is-open="isOpen" css-class="modal1" @onDidDismiss="handleDidDismiss(true)">
     <form>
-      <ion-item>
-        <ion-label position="floating">MM/DD/YYYY</ion-label>
-        <ion-datetime
-          v-model="date"
-          required
-          display-format="MM/DD/YYYY"
-          min="2021-05-12"
-          max="2025-12-30"
-        ></ion-datetime>
-      </ion-item>
-      <ion-item>
-        <ion-label position="floating">Password</ion-label>
-        <ion-input type="password" v-model="password" required></ion-input>
-      </ion-item>
-      <ion-button type="button" @click.prevent="submit();handleDidDismiss(false)">Submit</ion-button>
-      <ion-button @click.prevent="handleDidDismiss(false)">CLOSE</ion-button>
+      <ion-card>
+        <ion-item lines="none">
+          <ion-label color="medium" position="floating">Choose Start Time</ion-label>
+          <ion-datetime
+            v-model="date"
+            required
+            display-format="YYYY/MM/DD"
+            min="2021-05-12"
+            max="2025-12-30"
+          ></ion-datetime>
+        </ion-item>
+      </ion-card>
+      <ion-card>
+        <ion-item lines="none">
+          <ion-label color="medium" position="floating">Choose End Time</ion-label>
+          <ion-datetime
+            v-model="date"
+            required
+            display-format="YYYY/MM/DD"
+            min="2021-05-12"
+            max="2025-12-30"
+          ></ion-datetime>
+        </ion-item>
+      </ion-card>
+      <ion-card>
+        <ion-item lines="none">
+          <ion-label color="medium" position="floating">Password</ion-label>
+          <ion-input required type="password" v-model="password"></ion-input>
+        </ion-item>
+      </ion-card>
+      <ion-item id="agree" lines="none"><ion-label><p>Agree the User Agreement</p></ion-label></ion-item>
+      <div class="loginwrapper">
+        <ion-button
+          size="large"
+          expand="block"
+          type="button"
+          @click.prevent="submit();handleDidDismiss(false)"
+        >Rent the car</ion-button>
+      </div>
     </form>
   </ion-modal>
 </template>
@@ -29,6 +52,7 @@ import {
   IonDatetime,
   IonInput,
   IonButton,
+  IonCard,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 
@@ -36,7 +60,8 @@ export default defineComponent({
   name: "RentModal",
   data() {
     return {
-      date: null,
+      startdate: null,
+      enddate: null,
       password: "",
     };
   },
@@ -47,6 +72,7 @@ export default defineComponent({
     IonDatetime,
     IonInput,
     IonButton,
+    IonCard,
   },
   props: {
     isOpen: {
@@ -80,4 +106,22 @@ export default defineComponent({
 </script>
   
 <style scoped>
+ion-card {
+  box-shadow: none;
+  border-radius: 1rem;
+  margin: 32px;
+  border: 2px solid var(--ion-color-primary);
+}
+
+ion-button {
+  --border-radius: 1em;
+}
+
+.loginwrapper {
+  margin: 28px;
+}
+
+#agree{
+  margin-left: 32px;
+}
 </style>
