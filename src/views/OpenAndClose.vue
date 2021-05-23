fcc6bce47aaf651a5d1a1eb7c349e196e11779c0
MUTED <muted32555664@gmail.com>
2021-05-23 21:41:53 +0800

    Improved Bluetooth

--- src/views/OpenAndClose.vue
+++ src/views/OpenAndClose.vue
@@ -25,7 +25,12 @@
                 </div>
               </ion-col>
               <ion-col>
-                <ion-button class="right-bottom-button" expand="block" color="primary">OPEN</ion-button>
+                <ion-button
+                  class="right-bottom-button"
+                  expand="block"
+                  color="primary"
+                  @click="open()"
+                >OPEN</ion-button>
               </ion-col>
             </ion-row>
           </ion-grid>
@@ -49,6 +54,15 @@ import {
   IonTitle,
 } from "@ionic/vue";
 import "../theme/MyCars.scss";
+import {
+  BleClient,
+  textToDataView,
+  numberToUUID,
+} from "@capacitor-community/bluetooth-le";
+
+const HEART_RATE_SERVICE = "0000180d-0000-1000-8000-00805f9b34fb";
+const BODY_SENSOR_LOCATION_CHARACTERISTIC =
+  "00002a38-0000-1000-8000-00805f9b34fb";
 
 export default {
   components: {
@@ -62,6 +76,56 @@ export default {
     IonText,
     IonContent,
     IonTitle,
+  },
+  methods: {
+    async open() {
+      try {
+        await BleClient.initialize();
+
+        const device = await BleClient
+          .requestDevice
+          // services: [HEART_RATE_SERVICE],
+          // optionalServices: [BATTERY_SERVICE, POLAR_PMD_SERVICE],
+          ();
+
+        await BleClient.connect(device.deviceId);
+        console.log("connected to device", device);
+
+        const result = await BleClient.read(
+          device.deviceId,
+          HEART_RATE_SERVICE,
+          BODY_SENSOR_LOCATION_CHARACTERISTIC
+        );
+        console.log("body sensor location", result.getUint8(0));
+
+        await BleClient.write(
+          device.deviceId,
+          // POLAR_PMD_SERVICE,
+          // POLAR_PMD_CONTROL_POINT,
+          numberToUUID(0x181c),
+          numberToUUID(0x2a8a),
+          textToDataView("muted")
+        );
+        console.log("written muted");
+
+        const re = await BleClient.read(
+          device.deviceId,
+          numberToUUID(0x181c),
+          numberToUUID(0x2a8a)
+        );
+        console.log("name", re);
+        await BleClient.startNotifications(
+          device.deviceId,
+          HEART_RATE_SERVICE,
+          BODY_SENSOR_LOCATION_CHARACTERISTIC,
+          (value) => {
+            console.log("current value", value);
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
 };
 </script>
