8e21f52f18d2612cb8540b9345f02ded17bf5d51
MUTED <muted32555664@gmail.com>
2021-05-24 16:50:13 +0800

    Redesigned Exception Log

---
+++ src/components/ExpCars.vue
@@ -0,0 +1,55 @@
+<template>
+  <ion-card class="car">
+    <ion-card-title id="car-brand">PHIDEON</ion-card-title>
+    <ion-card-content id="car-name">Volkswagen</ion-card-content>
+
+    <div>
+      <img src="assets/car.png" />
+    </div>
+  </ion-card>
+</template>
+
+<script>
+import { IonCardContent, IonCard, IonCardTitle } from "@ionic/vue";
+
+export default {
+  components: {
+    IonCardContent,
+    IonCard,
+    IonCardTitle,
+  },
+};
+</script>
+
+<style scoped>
+img {
+  display: block;
+  width: 70%;
+  object-fit: cover;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -5%;
+}
+ion-card.car {
+  opacity: 97%;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+  min-height: 47vw;
+  border-radius: 1rem;
+  box-shadow: lightgray;
+}
+#car-brand {
+  height: 2rem;
+  margin-top: 4%;
+  margin-left: 7%;
+  letter-spacing: 1.5px;
+  font-size: 1.4rem;
+}
+#car-name {
+  height: 2rem;
+  margin-left: 7%;
+  padding: 0;
+  font-size: 1.2rem;
+}
+</style>
--- src/views/RunningState.vue
+++ src/views/RunningState.vue
@@ -1,14 +1,56 @@
 <template>
-  <base-layout page-title="行驶状况">
-  <no-violation></no-violation>
+  <base-layout buttonColor="light">
+    <ion-content>
+      <exp-cars class="card"></exp-cars>
+      <ion-title>
+        <h1>Exception Log</h1>
+      </ion-title>
+      <ion-card color="light" class="exception" v-if="!exception">
+        <ion-card-subtitle>
+          <h2 class="subtitle">No Exception</h2>
+        </ion-card-subtitle>
+      </ion-card>
+    </ion-content>
   </base-layout>
 </template>
 
 <script>
-import NoViolation from '../components/NoViolation.vue'
+import ExpCars from "../components/ExpCars.vue";
+import { IonCardSubtitle } from "@ionic/vue";
 
 export default {
-  components: { NoViolation },
-  
-}
+  data() {
+    return {
+      exception: undefined,
+    };
+  },
+  components: { ExpCars, IonCardSubtitle },
+};
 </script>
+
+<style scoped>
+ion-content {
+  width: 100%;
+  height: 100%;
+  --background: url("../../assets/exp.png") no-repeat 0 0 / 100% fixed, #fff;
+  background-size: 100% auto;
+  position: relative;
+}
+
+.card {
+  margin-top: 30vw;
+}
+
+.exception {
+  border-radius: 1rem;
+  text-align: center;
+  box-shadow: none;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+
+.subtitle {
+  color: var(--ion-color-medium);
+  margin: 7% auto;
+}
+</style>
